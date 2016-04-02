ff472f873d30d65f6b181328284cddfe2a1205be
Tjen Wellens <tjen.wellens@gmail.com>
2016-04-02 11:38:40 +0200

    color file-header yellow, chunk-position purple

--- script.sh
+++ script.sh
@@ -5,16 +5,26 @@ if tput setaf 1 &> /dev/null; then
 	reset=$(tput sgr0);
 	green=$(tput setaf 64);
 	red=$(tput setaf 124);
+	bold=$(tput bold);
+	yellow=$(tput setaf 136);
+	purple=$(tput setaf 125);
 else
 	reset="\e[0m";
 	green="\e[1;32m";
 	red="\e[1;31m";
+	bold='';
+	yellow="\e[1;33m";
+	purple="\e[1;35m";
 fi;
 
 while read line
 do
 	color="${reset}"
-	if [[ $line == \+* ]] ; then
+	if [[ $line == diff\ \-\-git* ]] || [[ $line == index\ * ]] || [[ $line == \-\-\-\ * ]] || [[ $line == \+\+\+\ * ]] ; then
+		color=${yellow}${bold}
+	elif [[ $line == \@\@\ * ]] ; then
+		color=${purple}${bold}
+	elif [[ $line == \+* ]] ; then
 		color=${green}
 	elif [[ $line == \-* ]] ; then
 		color=${red}
